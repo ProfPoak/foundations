3ae3d5c76a30bc858c7654be00e2b7722401bf56
Porter <pdhakes25@gmail.com>
2026-09-18 17:34:18 -0600

    implemented notes routes

--- server/api/notes/routes.py
+++ server/api/notes/routes.py
@@ -6,3 +6,41 @@ from config import db
 from models import Note, Customer
 from schema import NoteSchema, notes_schema
 
+class CustomerNotes(ProtectedResource):
+    def get(self, customer_id):
+        get_or_404(Customer, customer_id)
+        notes = Note.query.filter_by(customer_id=customer_id).order_by(Note.datetime.desc()).all()
+        return notes_schema.dump(notes), 200
+
+    def post(self, customer_id):
+        get_or_404(Customer, customer_id)
+        user = current_user()
+        request = request.get_json()
+        data = NoteSchema().load({**request,
+                                  "employee_id": user.id,
+                                  "customer_id": customer_id})
+        note = Note(**data)
+        db.session.add(note)
+        db.session.commit()
+        return NoteSchema.dump(note), 201
+
+class NoteDetail(ProtectedResource):
+    def patch(self, id):
+        note = get_or_404(Note, id)
+        if not can_modify(current_user(), note.employee_id):
+            return 403
+        r = request.get_json()
+        err = reject_unknown(r, ["content"])
+        db.session.commit()
+        return NoteSchema().dump(note), 200
+
+    def delete(self, id):
+        note = get_or_404(Note, id)
+        if not can_modify(current_user(), note.employee_id):
+            return 403
+        db.session.delete(note)
+        db.session.commit()
+        return "", 204
+
+notes_api.add_resource(CustomerNotes, "/customers/<int:customer_id>/notes")
+notes_api.add_resource(NoteDetail, "/notes/<int:id>")
