3127fb67c31ba3b611b2450360f7ab99cfe4dab5
Porter <pdhakes25@gmail.com>
2026-09-18 16:46:29 -0600

    implemented routes for CustomerEvents

--- server/api/events/routes.py
+++ server/api/events/routes.py
@@ -6,3 +6,22 @@ from api.helpers import ProtectedResource, get_or_404, current_user
 from config import db
 from models import Event, Customer
 from schema import EventSchema, events_schema
+
+class CustomerEvents(ProtectedResource):
+    def get(self, customer_id):
+        get_or_404(Customer, customer_id)
+        events = Event.query.filter_by(customer_id=customer_id).order_by(Event.datetime.desc()).all()
+        return events_schema.dump(events), 200
+
+    def post(self, customer_id):
+        get_or_404(Customer, customer_id)
+        user = current_user()
+        data = EventSchema().load({**request.get_json(),
+                                   "employee_id": user.id,
+                                   "customer_id": customer_id})
+        event = Event(**data)
+        db.session.add(event)
+        db.session. commit
+        return EventSchema().dump(event), 201
+
+events_api.add_resource(CustomerEvents, "/customers/<int:customer_id>/events")
