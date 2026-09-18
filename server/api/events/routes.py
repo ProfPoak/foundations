6c6c4b41c12154c2057191e7849ec30924905d6e
Porter <pdhakes25@gmail.com>
2026-09-18 16:36:19 -0600

    fixed imports for events routes

--- server/api/events/routes.py
+++ server/api/events/routes.py
@@ -1,8 +1,8 @@
 from flask import request
 from sqlalchemy.exc import IntegrityError
 
-from api.customers import customers_api
-from api.helpers import ProtectedResource, get_or_404
+from api.events import events_api
+from api.helpers import ProtectedResource, get_or_404, current_user
 from config import db
-from models import Customer
-from schema import CustomerSchema, customers_schema
+from models import Event, Customer
+from schema import EventSchema, events_schema
