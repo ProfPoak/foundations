c735dd4955bd8c885e074206e7e3d66e4b848264
Porter <pdhakes25@gmail.com>
2026-09-23 18:09:36 -0600

    Set up environment variable for database URL in tests; update config for database URI

--- server/config.py
+++ server/config.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
@@ -8,7 +10,8 @@ from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.secret_key = b'5a8fa62544c4912f1ad02547b49d1d5c7b0a9d4b353cf1253f74294c1b777b52'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+#Tests override this so they never touch the dev database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = b"771f1762607943b00d7111a488c20e2f15002f5fc054b071d6c2d6474d706205"
 app.json.compact = False
