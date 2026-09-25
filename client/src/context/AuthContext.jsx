b8ab5d44697ddb0b619e79ed234be9945d48fcb2
Porter <pdhakes25@gmail.com>
2026-09-24 22:03:16 -0600

    Implement AuthProvider with session management and authentication routes

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,10 +1,67 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
+import { apiFetch } from  '../api.js'
 
 //Value shape: { user, token, login(username, password), signup(username, password), logout() }
 const AuthContext = createContext(null)
 
 function AuthProvider({ children }) {
-  return <AuthContext.Provider value={null}>{children}</AuthContext.Provider>
+  const [user, setUser] = useState(null)
+  const [checkingSession, setCheckingSession] = useState(true)
+
+  //Session token check
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if(!token) {
+      setCheckingSession(false)
+      return
+    }
+
+    async function checkSession() {
+      const {ok, data} = await apiFetch('/check_session')
+
+      if (ok) {
+        setUser(data)
+      } 
+      else{
+        localStorage.removeItem('token')
+      }
+      
+      setCheckingSession(false)
+    }
+    checkSession()
+  }, [])
+
+  //Authentication routes
+  async function authRequests(path, username, password) {
+    const result = await apiFetch(path, {
+      method: 'POST',
+      body: JSON.stringify({ username, password})
+    })
+
+    if (result.ok) {
+      localStorage.setItem('token', result.data.token)
+      setUser(result.data.user)
+    }
+    
+    return result
+  }
+
+  function login(username, password) {
+    return authRequests('/login', username, password)
+  }
+
+  function signup(username, password) {
+    return authRequests('/signup', username, password)
+  }
+
+
+  function logout() {
+    localStorage.removeItem('token')
+    setUser(null)
+  }
+
+  return <AuthContext.Provider value={ {user, checkingSession, login, signup, logout} }>{children}</AuthContext.Provider>
 }
 
 function useAuth() {
